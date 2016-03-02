60b1a880eb213cf18699312e4e7fcce9b2ac630a
Harish.K <harish2704@gmail.com>
2016-03-02 19:07:38 +0530

    Fix: enable github markdown and table extensions

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,12 +11,11 @@ var MyApp = angular.module( 'MyApp', [
       // options to be passed to Showdown
       // see: https://github.com/coreyti/showdown#extensions
       markdownConverterProvider.config({
-        tables: true,
-        // extensions: ['twitter']
+        extensions: ['github', 'table']
       });
     }]);
 
-var API_METHODS = ['GET', 'POST'];
+var API_METHODS = ['GET', 'POST', 'PUT', 'DELETE' ];
 
 
 function Api(data){
